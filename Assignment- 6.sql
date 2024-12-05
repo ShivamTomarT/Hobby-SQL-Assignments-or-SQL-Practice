4178b7cff1604ece91744ee94a510b0c164d903b
ShivamTomarT <tomarspn5@gmail.com>
2024-12-06 00:58:10 +0530

    Add files via upload

---
+++ Assignment- 6.sql
@@ -0,0 +1,25 @@
+/********************************************************************************************************************
+
+                                                  Assignment- 6
+
+*********************************************************************************************************************/
+
+1.https://datalemur.com/questions/matching-skills
+
+2.https://datalemur.com/questions/sql-page-with-no-likes
+
+3.https://datalemur.com/questions/tesla-unfinished-parts
+
+4.https://datalemur.com/questions/laptop-mobile-viewership
+
+5.https://datalemur.com/questions/sql-average-post-hiatus-1
+
+6.https://datalemur.com/questions/teams-power-users
+
+7.https://datalemur.com/questions/completed-trades
+
+8.https://datalemur.com/questions/sql-avg-review-ratings
+
+9.https://datalemur.com/questions/click-through-rate
+
+10.https://datalemur.com/questions/second-day-confirmation
